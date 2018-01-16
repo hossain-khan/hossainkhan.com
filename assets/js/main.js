ebd8d18c5f93538ae330714282d8eac2e7f3d87d
Hossain Khan <amardeshbd@gmail.com>
2018-01-15 23:28:21 -0500

    [ADDED] Github calendar and feed links

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,11 +55,11 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "IonicaBizau");
+    GitHubCalendar("#github-graph", "amardeshbd");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "caseyscarborough", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "amardeshbd", selector: "#ghfeed" });
 
 
 });
