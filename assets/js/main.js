2c3cdfdc762514e22ac15c7457fdc8710e4ad820
Hossain Khan <amardeshbd@gmail.com>
2018-01-16 20:24:43 -0500

    [UPDATE] RSS feed to medium posts.

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,7 @@ jQuery(document).ready(function($) {
     $("#rss-feeds").rss(
     
         //Change this to your own rss feeds
-        "http://feeds.feedburner.com/TechCrunch/startups",
+        "https://medium.com/feed/@hossainkhan",
         
         {
         // how many entries do you want?
