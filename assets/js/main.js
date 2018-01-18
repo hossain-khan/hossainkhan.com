bc73157803cccf7e91d184a3d6deab983544dfb0
Hossain Khan <amardeshbd@gmail.com>
2018-01-18 18:59:08 -0500

    [REMOVED] Github activity plugin as it seemed redundant for the profile.

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,10 +56,4 @@ jQuery(document).ready(function($) {
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
     GitHubCalendar("#github-graph", "amardeshbd");
-    
-    
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "amardeshbd", selector: "#ghfeed" });
-
-
 });
