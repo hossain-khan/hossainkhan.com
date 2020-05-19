2e5c29c0dbc392d42fe32b2f5f0909224fd276df
Hossain Khan <amardeshbd@gmail.com>
2020-05-19 01:55:00 -0400

    [ADDED] Responsive mode for the Github Calendar

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,6 +59,6 @@ jQuery(document).ready(function($) {
         }
     );
     
-    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "amardeshbd");
+    /* Github Calendar - https://github.com/IonicaBizau/github-calendar or https://github.com/Bloggify/github-calendar/tree/1.3.4 */
+    GitHubCalendar("#github-graph", "amardeshbd", { responsive: true });
 });
