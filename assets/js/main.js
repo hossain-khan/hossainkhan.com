39e280eca9124df527548ed45dbdaeecb54fd888
Hossain Khan <hello@hossain.dev>
2024-12-13 13:30:39 -0500

    [ADDED] Conference section - last one

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,8 +41,8 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "IonicaBizau", { responsive: true });
+    new GitHubCalendar("#github-graph", "hossain-khan", { responsive: true });
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "mdo", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "hossain-khan", selector: "#ghfeed" });
